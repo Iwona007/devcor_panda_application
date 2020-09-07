67a323c9919a2814ccc6bedf4c201c33851bf247
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-09-07 18:13:52 +0000

    change pom and test

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -1,5 +1,4 @@
 package pl.pandait.panda;
-
 import java.net.URL;
 import java.net.MalformedURLException;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -8,34 +7,32 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-
 import static org.junit.Assert.assertEquals;
-
 
 @SpringBootTest(classes = {PandaApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class PandaApplicationSeleniumTest {
-
+    
     private static WebDriver driver;
-
+    
     @LocalServerPort
     private int port;
-
+    
     @BeforeEach
     public void startup() throws InterruptedException, MalformedURLException {
-
-        final FirefoxOptions capabilities = new FirefoxOptions();
-        capabilities.setCapability("marionette", true);
-        driver= new RemoteWebDriver(new URL("http://192.168.44.44:4444/wd/hub"), capabilities);
-    
-
+        
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new URL("http://192.168.44.44:4444/wd/hub"), capabilities);
         // Pamiętaj, że aplikacja Spring musi działać! To znaczy też musi być włączona.
-        driver.get(String.format("http://192.168.44.44:%d/", port));
+        
+        driver.get(String.format("http://ubuntu1:%d/", port));
 
         //Czekamy 2 sekundy
         Thread.sleep(2000);
@@ -44,26 +41,26 @@ public class PandaApplicationSeleniumTest {
     @Test
     public void greetings_shouldOpenMainPageThenReturnWelcomeText() {
         System.out.println("Uruchamiam test 1: Sprawdzenie napisu na stronie głównej");
-        final WebElement greetingElement = driver.findElement(By.xpath("//p"));
-        final String greetingText = greetingElement.getText().trim();
+        WebElement greetingElement = driver.findElement(By.xpath("//p"));
+        String greetingText = greetingElement.getText().trim();
         assertEquals("Get your greeting here", greetingText);
     }
 
     @Test
     public void greetings_shouldOpenSubpageThenReturnGreetingsText() {
         System.out.println("Uruchamiam test 2: Sprawdzenie napisu na podstronie");
-        final WebElement greetingElement = driver.findElement(By.xpath("//p"));
-        final WebElement linkToGreetings = greetingElement.findElement(By.xpath("./a"));
+        WebElement greetingElement = driver.findElement(By.xpath("//p"));
+        WebElement linkToGreetings = greetingElement.findElement(By.xpath("./a"));
         linkToGreetings.click();
 
-        final WebElement helloWorldString = driver.findElement(By.xpath("//p"));
-        final String newPageString = helloWorldString.getText().trim();
+        WebElement helloWorldString = driver.findElement(By.xpath("//p"));
+        String newPageString = helloWorldString.getText().trim();
         assertEquals("Hello, World!", newPageString);
     }
 
 
     @AfterEach
     public void after() {
-        driver.close();
+        driver.quit();
     }
 }
